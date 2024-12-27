ea89947b265b51605f3798f6da9418c1d9c8abaa
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2024-12-27 09:28:01 +0100

    Updated Version with new ui and all

---
+++ index.js
@@ -0,0 +1,169 @@
+var quoteList = [
+    "No! You're not fat, just stupid.",
+    "Hahaha, not wibe, It's vibe mf",
+    "You can't rush perfection Barbie chill, you got this!!",
+    "SHUT UP! You're good enough",
+    "Yes Barbie!!! You have muscles, shut up",
+    "You are not fat mf!!!! Don't be dumb",
+    "Gyonyoru Vagy",
+    "csinos vagy",
+    "Megbabonáz a szemed.",
+    "Szia, cica! Van gazdád?",
+    "Nagyon szeretlek",
+    "Meg tudod csinálni!",
+    "Gyerünk bazdmeg.",
+    "Mit veszíthetsz?",
+    "Ne add fel!",
+    "Higgy magadban!",
+    "Ez nagyon szeretelek",
+    "Gyonyoru Vagy, meg nagyon szeretlek",
+    "Ez az! let's go!",
+    "You look amazing!",
+    "I’m so lucky to be with you.",
+    "I love how smart  you are.",
+    "I love how kind you are.",
+    "I’m so glad you’re in my life.",
+    "You’re so fun to be with.",
+    "You got this, I believe in you.",
+    "I know I can always count on you.",
+    "You’re so fun to be with.",
+    "The way you do your things  is so inspiring!",
+    "You’re so special to me.",
+    "I see how hard you’ve been working on yourself ... you got this babe",
+
+    
+];
+
+
+
+
+
+
+document.querySelector(".btn").addEventListener("click", changeQuote);
+
+
+function changeQuote(){
+    var randomQuotes = quoteList[Math.floor(Math.random() * quoteList.length)] ;
+    document.querySelector(".random-quote").textContent = randomQuotes;
+}
+
+//console.log(randomQuotes);
+
+
+
+"You look amazing!",
+"I’m so lucky to be with you.",
+"I love that [shirt /hat/dress/etc.] on you!",
+"I love how smart  you are.",
+"I love how kind you are.",
+"I’m so glad you’re in my life.",
+"You’re so fun to be with.",
+"You got this, I believe in you."
+"I know I can always count on you.",
+"You’re so fun to be with.",
+"The way you do things  is so inspiring!",
+"You’re so special to me.",
+"I was so impressed when i see you",
+"I see how hard you’ve been working on yourself ... you got this babe"
+
+// I feel so grateful that I get to be with you.
+// I love our life together.
+// Thank you for listening.
+// I missed you.
+// I love how you get me.
+// I’m so happy to see you.
+// You’re the best.
+// No one makes me [laugh as much as/feel as happy/feel as beautiful] as you do.
+// You’re such a good partner.
+// You help me feel like I can do anything.
+// When I’m with you, I’m a better person.
+// You’re the best [parent /friend/teacher/etc.].
+// You handled that like a pro.
+// I value your judgement.
+// I’m so proud of you.
+// Your support means the world to me.
+// I love how you see the world.
+// Thank you for being a wonderful listener.
+// You broaden my perspective.
+// I hope you know how much you mean to me.
+// Being with you makes me so [happy /inspired/joyful]!
+// I love your [smile/laugh/eyes/way of seeing things].
+// Thank you for sharing your opinions with me.
+// I love that I can be myself with you.
+// Thank you for being honest with me.
+// You make me feel….
+// You’re so thoughtful.
+// You’re so kind.
+// You’re so sexy.
+// My life is more meaningful with you in it.
+// You rocked that situation.
+// Will you help me with something? Your input/advice is always so good.
+// I love how well you understand me.
+// You’re the best [gift-giver /cookie-baker/dad/mom/etc.].
+// You’re my best friend.
+// You’re one of my favorite people to be around.
+// You take such good care of me.
+// You always know the right thing to say.
+// You help me so much.
+// Thank you for being in my life.
+// We work so well together.
+// You’re an inspiration!
+// I love you.
+// You are my sunshine.
+// I’m sorry I hurt you.
+// I love that you did this for me.
+// Your being here makes everything better.
+// It must be so tough to deal with this. How can I help?
+// Great job on….
+// I wish I could … like you can.
+// You are amazing.
+// You inspire me.
+// Thank you for taking the time to do this with me.
+// You did that! Go you!
+// I learn so much from you.
+// I love your ideas.
+// You do so much for the people in your life.
+// I’m proud to be your partner.
+// I have total confidence in you.
+// I love how you make time for me.
+// I respect you.
+// I love being with you.
+// That was so thoughtful of you to do/say.
+// That was so kind of you to do/say.
+// You know how to make me laugh!
+// You know how to make me happy!
+// I feel safe with you.
+// You have the best sense of humor.
+// I love when we do something special together.
+// I’ll always be here for you.
+// You make everything more fun.
+// You’re such a good lover.
+// Thanks for seeing what I need/needed.
+
+// We make a great team.
+// Hello, gorgeous.
+// Hey there, sexy.
+// Our kids are so lucky to have you as their mom.
+// I trust your judgment.
+// Your opinion means so much to me.
+// You’re so considerate.
+// You really are the best.
+// You make so many things possible.
+// I love your company.
+// You’re a dream come true.
+// You’re one in a million.
+// Do you know how much I love you?
+// Things feel easier with you here.
+// You help me in so many ways.
+// I don’t know what I’d do without you.
+// I feel so lucky to have you in my life.
+// You’ve totally got this!
+// Your perspective is invaluable.
+// Wow! That was incredible!
+// Your [dinners /backrubs/ideas] are the best.
+// I love how your mind works.
+// Want to come with me?
+// I love how you see things.
+// Good thinking!
+// I appreciate you.
+
